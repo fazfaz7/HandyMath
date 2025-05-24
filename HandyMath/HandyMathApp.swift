967892a0d6d6e4dee93452e27d243273367eb19b
fazfaz7 <adrian.faz123@gmail.com>
2025-05-24 12:48:32 +0200

    Initial Commit

---
+++ HandyMath/HandyMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyMathApp.swift
+//  HandyMath
+//
+//  Created by Adrian Emmanuel Faz Mercado on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HandyMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
