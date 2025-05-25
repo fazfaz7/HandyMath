78987acc5b6c5f503a1a48cb4cb7023d8dc41974
fazfaz7 <adrian.faz123@gmail.com>
2025-05-25 18:08:03 +0200

    FIRST VERSION HANDY MATH

---
+++ HandyMath/HandPoseDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandPoseDetectionApp.swift
+//  HandPoseDetection
+//
+//  Created by Adrian Emmanuel Faz Mercado on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HandPoseDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
